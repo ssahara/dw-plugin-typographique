e8503521b1866035e5f3d36db179e4dc11b43e8a
Satoshi Sahara <sahara.satoshi@gmail.com>
2014-03-29 16:48:29 +0900

    rewrite code oop-like, add fc and bg properties

--- syntax/typography.php
+++ syntax/typography.php
@@ -1,115 +1,126 @@
 <?php
 /**
- * Plugin Typography
- * 
+ * DokuWiki Plugin Typographique; Syntax typographique typography
+ *
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * @author     Paweł Piekarski <qentinson@gmail.com>
+ * @author     Satoshi Sahara <sahara.satoshi@gmail.com>
  */
- 
+
 // must be run within Dokuwiki
 if(!defined('DOKU_INC')) die();
 if(!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
 require_once(DOKU_PLUGIN.'syntax.php');
- 
+
 class syntax_plugin_typographique_typography extends DokuWiki_Syntax_Plugin {
 
-	function getInfo(){
-		return array(
-            'author' => 'Paweł Piekarski',
-            'email'  => 'qentinson@gmail.com',
-            'date'   => '2011-01-18',
-            'name'   => 'Typography',
-            'desc'   => 'Empower dokuwiki with html typographic abilities.',
-            'url'    => 'http://piekarnia.edl.pl/projects/typography.html',
-		);
-	}
+    protected $entry_pattern = '<typo.*?>(?=.*?</typo>)';
+    protected $exit_pattern  = '</typo>';
 
-    function getType(){ return 'formatting'; }
-	function getSort() { return 667; }
-	function getAllowedTypes() { return array('formatting', 'substition', 'disabled'); }
-	
+    var $pluginMode, $props, $cond;
+
+    function __construct() {
+        $this->pluginMode = implode('_', array('plugin',$this->getPluginName(),$this->getPluginComponent(),));
+
+        $this->props = array(
+            'ff' => 'font-family:',
+            'fc' => 'color:',
+            'bg' => 'background-color:',
+            'fs' => 'font-size:',
+            'lh' => 'line-height:',
+            'ls' => 'letter-spacing:',
+            'ws' => 'word-spacing:',
+            'va' => 'vertical-align:',
+            'fv' => 'font-variant:',
+        );
+
+        $this->conds = array(
+            'ff' => '/^[a-zA-Z0-9\ ,]{2,}[a-zA-Z0-9]$/',
+            'fc' => '/(^\#([0-9a-fA-F]{3}|[0-9a-fA-F]{6})$)|'
+                   .'(^rgb\((\d{1,3}%?,){2}\d{1,3}%?\)$)|'
+                   .'(^[a-zA-Z]+$)/',
+            'bg' => '/(^\#([0-9a-fA-F]{3}|[0-9a-fA-F]{6})$)|'
+                   .'(^rgb\((\d{1,3}%?,){2}\d{1,3}%?\)$)|'
+                   .'(^rgba\((\d{1,3}%?,){3}[\d.]+\)$)|'
+                   .'(^[a-zA-Z]+$)/',
+            'fs' => '/(^\d+(?:\.\d+)?(px|em|ex|pt|%))$|'
+                   .'^(xx-small|x-small|small|medium|large|x-large|xx-large|smaller|larger)$/',
+            'lh' => '/^\d+(?:\.\d+)?(px|em|ex|pt|%)?$/',
+            'ls' => '/^-?\d+(?:\.\d+)?(px|em|ex|pt|%)$/',
+            'ws' => '/^-?\d+(?:\.\d+)?(px|em|ex|pt|%)$/',
+            'va' => '/^-?\d+(?:\.\d+)?(px|em|ex|pt|%)$|'
+                   .'^(baseline|sub|super|top|text-top|middle|bottom|text-bottom|inherit)$/',
+            'fv' => '/^smallcaps$/',
+        );
+    }
+
+    function getType(){  return 'formatting'; }
+    function getSort() { return 67; } // = Doku_Parser_Mode_formatting:strong -3
+    function getAllowedTypes() { return array('formatting', 'substition', 'disabled'); }
+
+    // Connect pattern to lexer
     function connectTo($mode) {
-		$this->Lexer->addEntryPattern('<typo (?:(?:ff:|fs:|lh:|ls:-?|ws:-?|va:-?|fv:){1}(?:[0-9]+px|[0-9]+(?:\.[0-9]+)?em|[0-9]+(?:\.[0-9]+)?ex|[0-9]+(?:\.[0-9]+)?pt|[0-9]{1,3}%|smallcaps{1}|baseline{1}|sub{1}|super{1}|top{1}|text-top{1}|middle{1}|bottom{1}|text-bottom{1}|inherit{1}|[a-zA-Z0-9]{2}[a-zA-Z0-9\ ,]*[a-zA-Z0-9]{1}){1};\ ?){1,6}>(?=.*?</typo>)', $mode,'plugin_typography');
-	}
-	function postConnect() {
-		$this->Lexer->addExitPattern('</typo>', 'plugin_typography');
-	}
- 
-	function handle($match, $state, $pos, &$handler) {
-		switch($state) {
-			case DOKU_LEXER_ENTER:
-				$conds = array(
-					'ff' => '/^[a-zA-Z0-9\ ,]{2,}[a-zA-Z0-9]$/',
-					'fs' => '/^[0-9]+(?:\.[0-9]+)?(px|em|ex|pt|%)$/',
-					'lh' => '/^[0-9]+(?:\.[0-9]+)?(px|em|ex|pt|%)$/',
-					'ls' => '/^-?[0-9]+(?:\.[0-9]+)?(px|em|ex|pt|%)$/',
-					'ws' => '/^-?[0-9]+(?:\.[0-9]+)?(px|em|ex|pt|%)$/',
-					'va' => '/(-?[0-9]+(?:\.[0-9]+)?(px|em|ex|pt|%)|baseline|sub|super|top|text-top|middle|bottom|text-bottom|inherit)/',
-					'fv' => '/^smallcaps$/',
-				);
-				$props = array(
-					'ff' => 'font-family:',
-					'fs' => 'font-size:',
-					'lh' => 'line-height:',
-					'ls' => 'letter-spacing:',
-					'ws' => 'word-spacing:',
-					'va' => 'vertical-align:',
-					'fv' => 'font-variant:',
-				);
-				$typo = trim(substr($match, 5, -1));
-				$tokens = explode(';', $typo);
-				$css = '';
+        $this->Lexer->addEntryPattern($this->entry_pattern, $mode,$this->pluginMode);
+    }
+    function postConnect() {
+        $this->Lexer->addExitPattern($this->exit_pattern, $this->pluginMode);
+    }
 
-				foreach($tokens as $t) {
-					if ($t == '') { continue; }
-					list($type, $val) = explode(':', trim($t));
-					if (!isset($conds[$type])) {
-						$css='';
-						break;
-					}
-					if (preg_match($conds[$type], $val)) {
-						if ($val == 'smallcaps') { $val = 'small-caps'; }
-						$css .= $props[$type].$val.' !important; ';
-					}
-					unset($conds[$type]); // do not use styling twice
-				}
-				if (!preg_match('/vertical-align:/', $css)) {
-					$css .= ' vertical-align: top; ';
-				}
+    /*
+     * Handle the match
+     */
+    function handle($match, $state, $pos, &$handler) {
 
-				if ($css != '') {
-					return array($state, $css);
-				}
+        switch($state) {
+            case DOKU_LEXER_ENTER:
+                $markup = substr($this->exit_pattern, 2, -1);
+                $params = strstr(substr($match, 1, -1), ' ');
 
-				return array($state, $match);
-			case DOKU_LEXER_UNMATCHED: return array($state, $match);
-			case DOKU_LEXER_EXIT: return array($state, '');
-		}
-		return array();
-	}
-	
-	function render($mode, &$renderer, $data) {
-		if ($mode != 'xhtml') return false;
+                if ($params == false) return array($state, '');
+                $tokens = explode(';', $params);
+                if ((count($tokens) == 1) && array_key_exists($markup, $this->props)) {
+                    // for inherited syntax class usage: <fs small>...</fs>
+                    $tokens[0] = $markup.':'.$tokens[0];
+                }
+                // msg('token:'.var_export($tokens, true), 0);
 
-		list($state, $match) = $data;
-		$render->doc .= $match;
-		switch ($state) {
-			case DOKU_LEXER_ENTER:
-				if ($match[0] == '<') {
-					$renderer->doc .= "<span>&lt;".substr($match, 1, -1).'&gt;';
-				} else {
-					$renderer->doc .= "<span style=\"$match\">";
-				}
-				break;
-			case DOKU_LEXER_UNMATCHED:
-				$renderer->doc .= $renderer->_xmlEntities($match);
-				break;
-			case DOKU_LEXER_EXIT:
-				$renderer->doc .= "</span>";
-				break;
-		}
-		return true;
-	}
+                $css = '';
+                foreach($tokens as $token) {
+                    if (empty($token)) continue;
+                    list($type, $val) = explode(':', trim($token));
+                    if (!array_key_exists($type, $this->props)) continue;
+                    if (preg_match($this->conds[$type], $val)) {
+                        if ($val == 'smallcaps') { $val = 'small-caps'; }
+                        $css .= $this->props[$type].$val.'; ';
+                    }
+                }
+                return array($state, $css);
+                break;
+            case DOKU_LEXER_UNMATCHED: return array($state, $match);
+            case DOKU_LEXER_EXIT:      return array($state, '');
+        }
+        return array();
+    }
+
+    /*
+     * Create output
+     */
+    function render($format, &$renderer, $data) {
+        if ($format != 'xhtml') return false;
+
+        list($state, $match) = $data;
+        switch ($state) {
+            case DOKU_LEXER_ENTER:
+                $renderer->doc .= '<span style="'.$match.'">';
+                break;
+            case DOKU_LEXER_UNMATCHED:
+                $renderer->doc .= $renderer->_xmlEntities($match);
+                break;
+            case DOKU_LEXER_EXIT:
+                $renderer->doc .= '</span>';
+                break;
+        }
+        return true;
+    }
 }
 
-?>
